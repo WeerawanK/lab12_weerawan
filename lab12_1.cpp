b162b7e8891c410d7f9d5d442ab91c5840643586
WeearwanK <weerawanpoint@gmail.com>
2024-12-24 16:03:03 +0700

    completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 
+unsigned int fibonacci(unsigned int x); 
+    
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+unsigned int fibonacci(unsigned int x){
+    if (x <= 1) {
+        return x;
+    } else {
+        return fibonacci(x - 1) + fibonacci(x - 2);
+    }
 }
